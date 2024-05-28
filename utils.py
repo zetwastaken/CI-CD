46e8d237c23140f9ce7e9a0b19bc96562fd21363
Zet <264193@student.pwr.edu.pl>
2024-05-28 11:45:52 +0200

    chore: Update Python workflow file formatting and dependencies

--- utils.py
+++ utils.py
@@ -1,19 +1,21 @@
-'''# przykladowe funkcje utils.py'''
+"""# przykladowe funkcje utils.py"""
+
+
 def add(a: int, b: int) -> int:
-    '''Add two numbers together and return the result.'''
+    """Add two numbers together and return the result."""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    '''Subtract two numbers and return the result.'''
+    """Subtract two numbers and return the result."""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    '''Multiply two numbers together and return the result.'''
+    """Multiply two numbers together and return the result."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    '''Divide two numbers and return the result.'''
+    """Divide two numbers and return the result."""
     return a / b
